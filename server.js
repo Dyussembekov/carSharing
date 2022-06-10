18545ab412769bf514f83be0032a3293bb0e3cf2
Dyussembekov <ndyusembekov07@gmail.com>
2022-06-10 19:37:34 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const dbConnection = require('./db')
+
+app.use(express.json())
+
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
+
+
+
+app.get('/', (req, res) => res.send('Hello NodeJS'))
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
